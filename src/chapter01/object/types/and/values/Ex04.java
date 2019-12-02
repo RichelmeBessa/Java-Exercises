ca26d9c1e1a7af7c065e665b0c510e0a0a9f7ff4
RichelmeBessa <richelmebessa@gmail.com>
2019-12-02 17:56:41 -0300

    Solve exercise04

--- src/chapter01/object/types/and/values/Ex04.java
+++ src/chapter01/object/types/and/values/Ex04.java
@@ -4,6 +4,30 @@
  */
 package chapter01.object.types.and.values;
 
+import java.util.Scanner;
+
 public class Ex04 {
-    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double val1;
+        double val2;
+        System.out.println("Write number floating");
+        val1 = Double.parseDouble(scanner.next());
+        System.out.println("Write other number floating");
+        val2 = Double.parseDouble(scanner.next());
+        if (val1 > val2) {
+            System.out.println("The biggest number is " + val1);
+            System.out.println("The smallest number is " + val2);
+        } else {
+            System.out.println("The biggest number is " + val2);
+            System.out.println("The smallest number is " + val1);
+        }
+
+        System.out.println("The sum of the numbers is equal to " + (val1 + val2));
+        System.out.println("The difference of the number is equal to " + (val1 - val2));
+        System.out.println("The product of the number is equal to " + (val1 * val2));
+        System.out.println("The ratio of the number is equal to " + (val1 / val2));
+    }
 }
+
