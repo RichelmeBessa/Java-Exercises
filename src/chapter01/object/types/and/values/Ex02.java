dcbdf50453c1ce0323ee98df60266400df797922
Derick Felix <derickfelix4321@gmail.com>
2019-12-02 14:22:41 -0500

    Declares exercise 02

---
+++ src/chapter01/object/types/and/values/Ex02.java
@@ -0,0 +1,9 @@
+/**
+ * Write a program that converts from miles to kilometers. Your program should
+ * have a reasonable prompt for the user to enter a number of miles.
+ */
+package chapter01.object.types.and.values;
+
+public class Ex02 {
+    
+}
