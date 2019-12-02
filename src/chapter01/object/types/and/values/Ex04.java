96984b980928e9db9c94662efa5d1872b7ba6571
Derick Felix <derickfelix4321@gmail.com>
2019-12-02 15:47:15 -0500

    Declare exercise 04

---
+++ src/chapter01/object/types/and/values/Ex04.java
@@ -0,0 +1,9 @@
+/**
+ * Modify the previous program to ask the user to enter floating-point values and
+ * and store them in double variables.
+ */
+package chapter01.object.types.and.values;
+
+public class Ex04 {
+    
+}
