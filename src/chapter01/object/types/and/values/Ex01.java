1b75de0591025bc340739f99589405b37d085327
Derick Felix <derickfelix4321@gmail.com>
2019-12-02 13:21:29 -0500

    Declare ex01

--- src/chapter01/object/types/and/values/Ex01.java
+++ src/chapter01/object/types/and/values/Ex01.java
@@ -1,9 +1,13 @@
+/**
+ * Write a program that prompts a first name and an age, then display the
+ * name the user entered with the following age in months.
+ * e.g:
+ * $ Please enter your first name and age: 
+ * > John 23
+ * "Hello, John Doe (age 276 months).
+ */
 package chapter01.object.types.and.values;
 
-/**
- *
- * @author richelme
- */
 public class Ex01 {
     
 }
