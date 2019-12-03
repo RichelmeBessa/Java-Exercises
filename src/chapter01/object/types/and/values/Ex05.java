adebd2a6c1c73f635429adfe9cd9a81219488e2c
RichelmeBessa <richelmebessa@gmail.com>
2019-12-03 15:46:12 -0300

    Solve exercise05

--- src/chapter01/object/types/and/values/Ex05.java
+++ src/chapter01/object/types/and/values/Ex05.java
@@ -7,6 +7,41 @@
  */
 package chapter01.object.types.and.values;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ex05 {
-    
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+
+        System.out.println("Enter first number integer: ");
+        a = scanner.nextInt();
+        System.out.println("Enter second number integer: ");
+        b = scanner.nextInt();
+        System.out.println("Enter third number integer:");
+        c = scanner.nextInt();
+        if (a <= b && b <= c){
+            System.out.println("The numbers ordener are: " + a + ", " + b + ", "+ c);
+        }
+        if (a <= c && c <= b) {
+            System.out.println("The numbers ordener are: " + a + ", " + c + ", " + b);
+        }
+        if (b <= a && a <= c) {
+            System.out.println("The numbers ordener are: " + b + ", " + a + ", " + c);
+        }
+        if (b <= c && c <= a) {
+            System.out.println("The numbers ordener are: " + b + ", " + c + ", " + a);
+        }
+        if (c <= b && b <= a) {
+            System.out.println("The numbers ordener are: " + c + ", " + b + ", " + a);
+        }
+        if (c <= a && a <= b) {
+            System.out.println("The numbers ordener are: " + c + ", " + a + ", " + b);
+        }
+    }
 }
