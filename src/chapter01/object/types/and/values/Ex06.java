193d6f8bfe859c68a30d47c84124700efd4e160d
Derick Felix <derickfelix4321@gmail.com>
2019-12-03 14:19:21 -0500

    Declare exercise 06

---
+++ src/chapter01/object/types/and/values/Ex06.java
@@ -0,0 +1,9 @@
+/**
+ * Do exercise 6, but with three strings values. So, if the user enters the values
+ * Steinbeck, Hemingway, Fitzgerald, the output should be Fitzgerald, Hemingway, Steinbeck.
+ */
+package chapter01.object.types.and.values;
+
+public class Ex06 {
+    
+}
