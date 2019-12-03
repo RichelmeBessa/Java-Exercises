d48d0f8ee7838ef497d7ea3e8cc38622fcc1ddc8
Derick Felix <derickfelix4321@gmail.com>
2019-12-03 15:37:44 -0500

    Declare exercise 08

---
+++ src/chapter01/object/types/and/values/Ex08.java
@@ -0,0 +1,11 @@
+/**
+ * Write a program that converts spelled-out numbers such as "zero" and
+ * "two" into Such as 0 and 2. Do it for the values 1, 2, 3, and 4 and
+ * write out not a number I know if the user enters something that 
+ * doesn't correspond, such as stupid computer!.
+ */
+package chapter01.object.types.and.values;
+
+public class Ex08 {
+    
+}
