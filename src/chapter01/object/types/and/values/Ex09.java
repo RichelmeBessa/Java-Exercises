747bc780271fed086ecc68d3ea831de69c0d2596
Derick Felix <derickfelix4321@gmail.com>
2019-12-04 11:51:28 -0500

    Declare exercise 09

---
+++ src/chapter01/object/types/and/values/Ex09.java
@@ -0,0 +1,10 @@
+/**
+ * Write a program that takes an operation followed by two operands and outputs the
+ * result. For example:
+ * 100 + 3.14
+ */
+package chapter01.object.types.and.values;
+
+public class Ex09 {
+    
+}
