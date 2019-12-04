bba9638086835f5c7940f287c8c83fc88f15aee6
RichelmeBessa <richelmebessa@gmail.com>
2019-12-04 17:01:01 -0300

    Solve exercise09

--- src/chapter01/object/types/and/values/Ex09.java
+++ src/chapter01/object/types/and/values/Ex09.java
@@ -11,31 +11,41 @@ public class Ex09 {
 
     public static void main(String[] args)
     {
-        int a1;
-        int a2;
-        String op;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a sum: ");
-        a1 = scanner.nextInt();
-        op = scanner.next();
-        a2 = scanner.nextInt();
-        int result = 0;
-        switch (op) {
-            case "+":
-                result = (a1 + a2);
+
+        System.out.println("Enter a number followed by one of these operations and then enter another number.");
+        System.out.println("It is noteworthy that there must be spaces between them. Press P, pause");
+
+        while (true) {
+            String temp = scanner.next();
+            
+            if (temp.equals("p")) {
                 break;
-            case "-":
-                result = (a1 - a2);
-                break;
-            case "*":
-                result = (a1 * a2);
-                break;
-            case "/":
-                result = (a1 / a2);
-                break;
-            default:
-                break;
+            }
+            
+            int a1 = Integer.parseInt(temp);
+            String op = scanner.next();
+            int a2 = scanner.nextInt();
+            
+            int result = 0;
+            
+            switch (op) {
+                case "+":
+                    result = (a1 + a2);
+                    break;
+                case "-":
+                    result = (a1 - a2);
+                    break;
+                case "*":
+                    result = (a1 * a2);
+                    break;
+                case "/":
+                    result = (a1 / a2);
+                    break;
+                default:
+                    break;
+            }
+            System.out.println(result);
         }
-        System.out.println(result);
     }
 }
