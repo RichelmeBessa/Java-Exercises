143329b75ec36420fa5b590385ccd6a23a972ad1
Derick Felix <derickfelix4321@gmail.com>
2019-12-04 15:03:35 -0500

    Declare execise 10

---
+++ src/chapter01/object/types/and/values/Ex10.java
@@ -0,0 +1,12 @@
+/**
+ * Write a program that prompts the user to enter some number of pennies,
+ * nickels, dimes, quarters, half-dollars, and one dollar. Query the user
+ * separately for the numbers of each coin. If only one of a coin is
+ * reported, make the output grammatically correct. Also, report the sum
+ * in dollars and cents.
+ */
+package chapter01.object.types.and.values;
+
+public class Ex10 {
+    
+}
