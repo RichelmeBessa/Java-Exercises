86142ecb5dbd0a7ed88cf03c671638f6b738e691
RichelmeBessa <richelmebessa@gmail.com>
2019-12-04 18:04:51 -0300

    Solve exercise10

--- src/chapter01/object/types/and/values/Ex10.java
+++ src/chapter01/object/types/and/values/Ex10.java
@@ -7,6 +7,34 @@
  */
 package chapter01.object.types.and.values;
 
+import java.util.Scanner;
+
 public class Ex10 {
-    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a number pennies, nickels, dimes , quarters, hald-dollars and dollar.");
+        System.out.println("Value pennies: ");
+        int pennies = scanner.nextInt();
+        System.out.println("Value nickels: ");
+        int nickels = scanner.nextInt();
+        System.out.println("Value dimes: ");
+        int dimes = scanner.nextInt();
+        System.out.println("Value quarters: ");
+        int quarters = scanner.nextInt();
+        System.out.println("Value half-dollar: ");
+        int half = scanner.nextInt();
+        System.out.println("Value dollar: ");
+        int dollar = scanner.nextInt();
+        
+        System.out.println("You have: " + pennies + " pennies");
+        System.out.println("You have: " + nickels + " nickels");
+        System.out.println("You have: " + dimes + " dimes");
+        System.out.println("You have: " + quarters + " quarters");
+        System.out.println("You have: " + half + " half-dollars");
+        System.out.println("You have: " + dollar + " dollars");
+        System.out.println(pennies*0.01 + nickels*0.05 + dimes*0.1 + quarters*0.25 + half*0.5 + dollar);
+        
+    }
 }
