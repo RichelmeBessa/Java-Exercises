90364b4d6af820efe6b4c07249b2d93af942612f
RichelmeBessa <richelmebessa@gmail.com>
2019-12-04 13:48:07 -0300

    Solve exercise08

--- src/chapter01/object/types/and/values/Ex08.java
+++ src/chapter01/object/types/and/values/Ex08.java
@@ -1,7 +1,7 @@
 /**
  * Write a program that converts spelled-out numbers such as "zero" and
  * "two" into Such as 0 and 2. Do it for the values 1, 2, 3, and 4 and
- * write out not a number I know if the user enters something that 
+ * write out not a number I know if the user enters something that
  * doesn't correspond, such as stupid computer!.
  */
 package chapter01.object.types.and.values;
@@ -9,6 +9,7 @@ package chapter01.object.types.and.values;
 import java.util.Scanner;
 
 public class Ex08 {
+
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
@@ -17,16 +18,14 @@ public class Ex08 {
         spelled = scanner.nextLine();
         if (spelled.equals("one")) {
             System.out.println("1");
-        }
-        if (spelled.equals("two")){
+        } else if (spelled.equals("two")) {
             System.out.println("2");
-        }
-        if (spelled.equals("three")){
+        } else if (spelled.equals("three")) {
             System.out.println("3");
-        }
-        if (spelled.equals("four")){
+        } else if (spelled.equals("four")) {
             System.out.println("4");
+        } else {
+            System.out.println("you are dumb ass");
         }
-       
     }
 }
