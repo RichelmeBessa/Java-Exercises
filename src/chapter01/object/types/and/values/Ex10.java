95141a72c9f60e0720b9f2297f0df0d9fc829978
RichelmeBessa <richelmebessa@gmail.com>
2019-12-05 15:33:16 -0300

    Solve exercise10

--- src/chapter01/object/types/and/values/Ex10.java
+++ src/chapter01/object/types/and/values/Ex10.java
@@ -16,55 +16,71 @@ public class Ex10 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number pennies, nickels, dimes , quarters, hald-dollars and dollar.");
+
         System.out.println("Value pennies: ");
-        int pennies = scanner.nextInt();
+        double pennies = scanner.nextDouble();
         System.out.println("Value nickels: ");
-        int nickels = scanner.nextInt();
+        double nickels = scanner.nextDouble();
         System.out.println("Value dimes: ");
-        int dimes = scanner.nextInt();
+        double dimes = scanner.nextDouble();
         System.out.println("Value quarters: ");
-        int quarters = scanner.nextInt();
+        double quarters = scanner.nextDouble();
         System.out.println("Value half-dollar: ");
-        int half = scanner.nextInt();
+        double half = scanner.nextDouble();
         System.out.println("Value dollar: ");
-        int dollar = scanner.nextInt();
-        
+        double dollar = scanner.nextDouble();
+
         if (pennies > 1) {
             System.out.println("You have: " + pennies + " pennies");
         } else {
             System.out.println("You have: " + pennies + " pennie");
         }
+
         if (nickels > 1) {
             System.out.println("You have: " + nickels + " nickels");
 
         } else {
             System.out.println("You have: " + nickels + " nickel");
         }
+
         if (dimes > 1) {
             System.out.println("You have: " + dimes + " dimes");
         } else {
             System.out.println("You have: " + dimes + " dime");
         }
+
         if (quarters > 1) {
             System.out.println("You have: " + quarters + " quarters");
 
         } else {
             System.out.println("You have: " + quarters + " quarter");
         }
+
         if (half > 1) {
             System.out.println("You have: " + half + " half-dollars");
 
         } else {
             System.out.println("You have: " + half + " half-dollar");
         }
+
         if (dollar > 1) {
             System.out.println("You have: " + dollar + " dollars");
 
         } else {
             System.out.println("You have: " + dollar + " dollar");
         }
-        
-        System.out.println(pennies * 0.01 + nickels * 0.05 + dimes * 0.1 + quarters * 0.25 + half * 0.5 + dollar);
 
+        double result = 0;
+        result += pennies * 0.01;
+        result += nickels * 0.05;
+        result += dimes * 0.1;
+        result += quarters * 0.25;
+        result += half * 0.5;
+        result += dollar;
+
+        int finish = (int) result;
+
+        System.out.printf("You have %d dollars and %.2s cents\n", finish, (result - finish) * 100);
+        scanner.close();
     }
 }
