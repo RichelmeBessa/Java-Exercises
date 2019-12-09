e14aadb14367c29aacb8383816ea90e334a9b8bc
derickfelix <derickfelix4321@gmail.com>
2019-12-10 06:43:23 +1100

    Declare exercise 04

---
+++ src/chapter02/computation/Ex04.java
@@ -0,0 +1,6 @@
+/*
+* Write a program to play a numbers guessing game. The user thinks a number
+* between 1 and 100 and your program ask questions to figure out what the number
+* is. Your program should be able to identify the number after asking no more
+* than seven questions.
+*/
