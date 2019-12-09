8f75b4ad377c68bd587d86c82c81ff8d2da395ad
RichelmeBessa <richelmebessa@gmail.com>
2019-12-09 16:35:03 -0300

    Solve exercise03

--- src/chapter02/computation/Ex03.java
+++ src/chapter02/computation/Ex03.java
@@ -5,3 +5,45 @@
  * the smallest and greatest distance between two neighboring cities.
  * Find and print the mean distance between two neighboring cities.
  */
+package chapter02.computation;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Ex03 {
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Vector<Double> vector = new Vector<>();
+        double total = 0;
+        System.out.println("Follow the instruction above. Press 'x' to pause.");
+        
+        while (true) {
+            System.out.println("Enter a distance");
+            String input = scanner.next();
+            
+            if (input.equals("x")){
+                break;
+            }
+            vector.add(Double.parseDouble(input));
+        }
+        double maior = 0;
+        double menor = Double.MAX_VALUE;
+        for (Double distance : vector){
+            total += distance;
+            if (distance > maior){
+                maior = distance;
+            } 
+            if (distance < menor){
+                menor = distance;
+            }
+            
+        }
+        Collections.sort(vector);
+        System.out.println("The sum of distance is: " + total );
+        System.out.println(menor);
+        System.out.println(maior);
+    }
+}
