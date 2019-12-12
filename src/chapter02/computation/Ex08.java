ba356f195b1786c571ceb356df88ad617cc09153
RichelmeBessa <richelmebessa@gmail.com>
2019-12-12 16:03:57 -0300

    Solve exercise08

--- src/chapter02/computation/Ex08.java
+++ src/chapter02/computation/Ex08.java
@@ -7,9 +7,18 @@
  * and at least 1,000,000,000 grains.
  */
 package chapter02.computation;
+
+import java.util.Scanner;
+
 public class Ex08{
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        int square;
+        while (true){
+        System.out.println("Enter with a number for it to be similar to the emperor who referred to rice grains. ");
+        square = scanner.nextInt();
+        System.out.println(Math.pow(2, square-1)); 
+        }
     }
 }
