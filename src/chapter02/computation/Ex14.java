0c51d3bfe9c3b4f4763397b515d3a7c339fd706e
RichelmeBessa <richelmebessa@gmail.com>
2019-12-13 18:05:58 -0300

    Solve exercise14

--- src/chapter02/computation/Ex14.java
+++ src/chapter02/computation/Ex14.java
@@ -3,9 +3,65 @@
  * is called the mode. Create a program that finds the mode,
  * the average and the median of a set of positive integers
  */
+package chapter02.computation;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
+public class Ex14 {
 
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter with a set of positive integers separeted by blank space (Press 'x' to finish): ");
+        List<Integer> numbers = new ArrayList<>();
 
- 
+        while (true) {
+            String input = scan.next();
+
+            if (input.equals("x")) {
+                break;
+            }
+
+            numbers.add(Integer.parseInt(input));
+        }
+        int total = 0;
+
+        for (Integer number : numbers) {
+            total += number;
+        }
+
+        System.out.println("The average is: " + total / numbers.size());
+
+        if (numbers.size() % 2 == 0) {
+            int a = numbers.get(numbers.size() / 2 - 1);
+            int b = numbers.get(numbers.size() / 2);
+            System.out.println("The median is: " + (a + b) / 2);
+
+        } else {
+            System.out.println("The median is: " + numbers.get(numbers.size() / 2));
+        }
         
+        int maior = 0;
+        int moda = 0;
+        
+        for (Integer number : numbers) {
+            int count = 0;           
+            for (Integer namber : numbers) {
+                if(namber == number){
+                    count++;
+                }
+            }
+            
+            if(count > maior){
+              maior = count;
+              moda = number;
+            }
+        }
+
+        
+        System.out.println("The mode is: " + moda);
+    }
+}
