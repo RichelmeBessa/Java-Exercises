1c9ff3a91e14677cfd6bc187b7c82aca65a09634
RichelmeBessa <richelmebessa@gmail.com>
2019-12-16 15:18:48 -0300

    Solve exercise

--- src/chapter02/computation/Ex16.java
+++ src/chapter02/computation/Ex16.java
@@ -1,5 +1,55 @@
 /**
  * Write a program that finds the min, max and mode of a sequence of strings.
  */
-    
-     
+package chapter02.computation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex16 {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        List<String> sequences = new ArrayList<>();
+
+        System.out.println("Enter with a sequence of strings. Press '.' to stop.");
+
+        while (true) {
+            String temp = scan.next();
+            if (temp.equals(".")) {
+                break;
+            }
+            sequences.add(temp);
+        }
+
+        sequences.sort((a, b) -> a.compareTo(b));
+
+        int countMode = 0;
+        String mode = null;
+
+        for (String a : sequences) {
+            int countA = 0;
+
+            for (String b : sequences) {
+                if (a.equals(b)) {
+                    countA++;
+                }
+            }
+
+            if (countA > countMode) {
+                countMode = countA;
+                mode = a;
+            }
+        }
+        if (!sequences.isEmpty()) {
+            System.out.println(
+                    "The smallest is " + sequences.get(0));
+            System.out.println(
+                    "The biggest is: " + sequences.get(sequences.size() - 1));
+        }
+
+        System.out.println("The mode: " + (countMode > 1 ? mode : "doesn't exist."));
+    }
+}
